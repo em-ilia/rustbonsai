7ab77d2f1bd2fe1f125274a537616e6ef50d1dba
Emilia <contact@emiliaallison.com>
2023-03-25 21:00:10 -0400

    Revise Screen struct so that origin in bottom middle of window

--- src/main.rs
+++ src/main.rs
@@ -53,25 +53,25 @@ impl Screen {
     }
     pub fn draw_str(&self, x: i16, y: i16, s: &str) {
         let x_adj = x + self.x_max as i16/2 ;
-        let y_adj = y + self.y_max as i16/2 ;
+        let y_adj = self.y_max as i16 - y;
         if x_adj as u16 > self.x_max {return ()};
         if y_adj as u16 > self.y_max {return ()};
 
-        queue!(stdout(), MoveTo(x as u16, y as u16), Print(s));
+        queue!(stdout(), MoveTo(x_adj as u16, y_adj as u16), Print(s));
     }
 }
-
 
 fn ui_loop() -> Result<()> {
     let mut t = tree::Tree::default();
     let scr = Screen::new();
 
-    for _ in 1..=30 {
+    for _ in 1..=60 {
         t.grow();
         for (x,y,s) in t.observe() {
             scr.draw_str(x as i16, y as i16, s);
+            thread::sleep(Duration::from_millis(20));
+            stdout().flush()?;
         }
-        stdout().flush();
     }
     thread::sleep(Duration::from_secs(1));
 
