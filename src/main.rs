91cd5f0d0418e67f746abbd794b9307156f3a58d
Emilia <contact@emiliaallison.com>
2023-03-27 23:26:22 -0400

    Run rustfmt
    It still compiles and runs, so it can't have been that bad

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod tree;
+use crossterm::{
+    cursor::{Hide, MoveTo, Show},
+    execute, queue,
+    style::Print,
+    terminal::{
+        disable_raw_mode, enable_raw_mode, size, EnterAlternateScreen, LeaveAlternateScreen,
+    },
+    Result,
+};
 use std::{
     io::{stdout, Write},
+    thread,
     time::Duration,
-    thread
-};
-use crossterm::{
-    execute,
-    queue,
-    Result,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode, size},
-    cursor::{Hide, Show, MoveTo},
-    style::Print
 };
 
 fn main() -> Result<()> {
@@ -32,18 +33,22 @@ fn main() -> Result<()> {
 
 struct Screen {
     pub x_max: u16,
-    pub y_max: u16
+    pub y_max: u16,
 }
 impl Screen {
     pub fn new() -> Self {
-        let (x,y) = size().unwrap();
+        let (x, y) = size().unwrap();
         Screen { x_max: x, y_max: y }
     }
     pub fn draw_str(&self, x: i16, y: i16, s: &str) {
-        let x_adj = x + self.x_max as i16/2 ;
+        let x_adj = x + self.x_max as i16 / 2;
         let y_adj = self.y_max as i16 - y;
-        if x_adj as u16 > self.x_max {return ()};
-        if y_adj as u16 > self.y_max {return ()};
+        if x_adj as u16 > self.x_max {
+            return ();
+        };
+        if y_adj as u16 > self.y_max {
+            return ();
+        };
 
         queue!(stdout(), MoveTo(x_adj as u16, y_adj as u16), Print(s));
     }
@@ -56,7 +61,7 @@ fn ui_loop() -> Result<()> {
 
     for _ in 1..=60 {
         t.grow();
-        for (x,y,s) in t.observe() {
+        for (x, y, s) in t.observe() {
             scr.draw_str(x as i16, y as i16, s);
             thread::sleep(Duration::from_millis(20));
             stdout().flush()?;
--- src/tree.rs
+++ src/tree.rs
@@ -8,7 +8,7 @@ pub struct Tree {
     pub state: TreeState,
     knots: Vec<Tree>,
     xmax: f64,
-    ymax: f64
+    ymax: f64,
 }
 #[derive(PartialEq, Debug)]
 pub enum TreeState {
@@ -16,7 +16,7 @@ pub enum TreeState {
     BranchLeft,
     BranchRight,
     Leaves,
-    Dead
+    Dead,
 }
 impl Tree {
     pub fn new(xmax: f64, ymax: f64) -> Self {
@@ -27,7 +27,7 @@ impl Tree {
             state: TreeState::Trunk,
             knots: Vec::new(),
             xmax,
-            ymax
+            ymax,
         }
     }
     fn new_at(&self) -> Self {
@@ -38,7 +38,7 @@ impl Tree {
             state: TreeState::Trunk, // Should have RNG here
             knots: Vec::new(),
             xmax: self.xmax,
-            ymax: self.ymax
+            ymax: self.ymax,
         }
     }
 
@@ -55,16 +55,19 @@ impl Tree {
             TreeState::Trunk => trunk_growth(self),
             TreeState::BranchLeft => left_shoot_growth(self),
             TreeState::BranchRight => right_shoot_growth(self),
-            TreeState::Dead => (0,0),
-            TreeState::Leaves => (0,0) // Not sure what to do here
+            TreeState::Dead => (0, 0),
+            TreeState::Leaves => (0, 0), // Not sure what to do here
         };
         self.x += d.0 as f64;
         self.y += d.1 as f64;
 
         // State transitions
         if self.state == TreeState::Trunk && thread_rng().gen_ratio(1, 15) {
-            self.state = if thread_rng().gen_bool(0.5) {TreeState::BranchLeft}
-            else {TreeState::BranchRight};
+            self.state = if thread_rng().gen_bool(0.5) {
+                TreeState::BranchLeft
+            } else {
+                TreeState::BranchRight
+            };
         }
 
         // Occasionally create a knot
@@ -91,7 +94,7 @@ impl Tree {
 
 /// trunk_growth takes a tree in
 /// a trunk state and returns:
-/// * a dx integer, the amount to grow 
+/// * a dx integer, the amount to grow
 /// UNTESTED
 fn trunk_growth(t: &Tree) -> (i16, i16) {
     // New trunk:
@@ -100,14 +103,13 @@ fn trunk_growth(t: &Tree) -> (i16, i16) {
         0..=3 => return (thread_rng().gen_range(0..=2), 0),
         4..=15 => {
             let x = thread_rng().gen_range(-1..=2);
-            let y = if (t.age % 3 == 0) {1} else {0};
-            return (x,y);
+            let y = if (t.age % 3 == 0) { 1 } else { 0 };
+            return (x, y);
         }
         _ => {
             let x = thread_rng().gen_range(-1..=2);
-            let y = if thread_rng().gen_ratio(1, 5)
-                            {1} else {0};
-            return (x,y);
+            let y = if thread_rng().gen_ratio(1, 5) { 1 } else { 0 };
+            return (x, y);
         }
     }
 }
@@ -117,17 +119,17 @@ fn left_shoot_growth(t: &Tree) -> (i16, i16) {
     let mut x: i16 = 0;
     let mut y: i16 = 0;
     match thread_rng().gen_range(1..=10) {
-        1|2 => y=-1,
-        9|10 => y=1,
-        _ => ()
+        1 | 2 => y = -1,
+        9 | 10 => y = 1,
+        _ => (),
     }
     match thread_rng().gen_range(1..=10) {
-        1|2 => x = -2,
+        1 | 2 => x = -2,
         3..=6 => x = -1,
         7..=9 => x = 0,
-        _ => x = 1
+        _ => x = 1,
     }
-    return (x,y)
+    return (x, y);
 }
 
 /// Same as trunk_growth
@@ -135,52 +137,60 @@ fn right_shoot_growth(t: &Tree) -> (i16, i16) {
     let mut x: i16 = 0;
     let mut y: i16 = 0;
     match thread_rng().gen_range(1..=10) {
-        1|2 => y=-1,
-        9|10 => y=1,
-        _ => ()
+        1 | 2 => y = -1,
+        9 | 10 => y = 1,
+        _ => (),
     }
     match thread_rng().gen_range(1..=10) {
-        1|2 => x = 2,
+        1 | 2 => x = 2,
         3..=6 => x = 1,
         7..=9 => x = 0,
-        _ => x = -1
+        _ => x = -1,
     }
-    return (x,y)
+    return (x, y);
 }
 
-const TRUNK_STRINGS: [&str;4] = ["/~","\\|","/|\\","|/"];
-const SHOOT_STRINGS: [&str;6] = ["\\","\\_","\\|","/|","/","_/"];
+const TRUNK_STRINGS: [&str; 4] = ["/~", "\\|", "/|\\", "|/"];
+const SHOOT_STRINGS: [&str; 6] = ["\\", "\\_", "\\|", "/|", "/", "_/"];
 fn choose_string(t: &Tree) -> &str {
     match t.state {
         TreeState::Trunk => {
             let d = trunk_growth(t);
-            if (d.1 == 0) {return TRUNK_STRINGS[0]};
+            if (d.1 == 0) {
+                return TRUNK_STRINGS[0];
+            };
             match d.0 {
                 n if n < 0 => return TRUNK_STRINGS[1],
                 0 => return TRUNK_STRINGS[2],
-                _ => return TRUNK_STRINGS[3]
+                _ => return TRUNK_STRINGS[3],
             }
-        },
+        }
         TreeState::BranchLeft => {
             let d = left_shoot_growth(t);
-            if (d.1 > 0) {return SHOOT_STRINGS[0]}
-            else if d.1 == 0 {return SHOOT_STRINGS[1]};
+            if (d.1 > 0) {
+                return SHOOT_STRINGS[0];
+            } else if d.1 == 0 {
+                return SHOOT_STRINGS[1];
+            };
             match d.0 {
                 n if n < 0 => return SHOOT_STRINGS[2],
                 0 => SHOOT_STRINGS[3],
-                _ => SHOOT_STRINGS[4]
+                _ => SHOOT_STRINGS[4],
             }
-        },
+        }
         TreeState::BranchRight => {
             let d = right_shoot_growth(t);
-            if (d.1 > 0) {return SHOOT_STRINGS[4]}
-            else if d.1 == 0 {return SHOOT_STRINGS[5]};
+            if (d.1 > 0) {
+                return SHOOT_STRINGS[4];
+            } else if d.1 == 0 {
+                return SHOOT_STRINGS[5];
+            };
             match d.0 {
                 n if n < 0 => return SHOOT_STRINGS[2],
                 0 => SHOOT_STRINGS[3],
-                _ => SHOOT_STRINGS[4]
+                _ => SHOOT_STRINGS[4],
             }
-        },
-        _ => {return "shrimp"}
+        }
+        _ => return "shrimp",
     }
 }
